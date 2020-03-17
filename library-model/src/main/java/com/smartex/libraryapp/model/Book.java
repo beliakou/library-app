1413ccf9e2165b6533f38f48ace13a6a95f27385
viktor <viktor_beliakov@smartexlab.com>
2020-03-17 12:28:02 +0300

    add model

---
+++ library-model/src/main/java/com/smartex/libraryapp/model/Book.java
@@ -0,0 +1,52 @@
+package com.smartex.libraryapp.model;
+
+import java.util.Set;
+
+public class Book {
+
+    private Long id;
+    private String isbn;
+    private String name;
+    private Author author;
+    private Set<Category> categories;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+}
